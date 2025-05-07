cc1a4319039b3c5ab9b0c38fad5b25aa0abd6a98
anamaydev <anamaydev@gmail.com>
2025-05-08 00:56:34 +0530

    feat: add finished mobile UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,64 @@
 /*ToDO
 *  [ ] fetch Data only onload
-*  [ ] create selectedLanguage (state) initialise with empty string
+*  [x] create selectedLanguage (state)
 *  [ ] create basic frame header + question section + options section
+*  [ ] add shadows
+*  [ ] add "selected-option" to selected option
+*  [ ] add "correct-option" to selected option
+*  [ ] add "incorrect-option" to selected option
+*  [ ] check alt for all images/icons
 * */
 
 import {useState} from 'react';
 import {getIconUrl} from "./utils.js";
+import clsx from "clsx";
 import data from '../data.json';
 import './App.css'
+import correctIcon from './assets/images/icon-correct.svg'
+import DarkMoonIcon from './assets/images/icon-moon-dark.svg'
+import DarkSunIcon from './assets/images/icon-sun-dark.svg'
 
 function App() {
   // state variables
-  const [selectedLanguage, setSelectedLanguage] = useState(3);
+  const [selectedLanguage, setSelectedLanguage] = useState(0);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   // derived variables
   const quizData = data.quizzes;
-  const iconPath = quizData[selectedLanguage].icon;
-  console.log(`iconPath: ${iconPath}`);
 
   return (
     <>
       {
-        selectedLanguage >= 0 &&
+        selectedLanguage !== null &&
         <header className="header">
           <div className="header__language">
-            <img className="header__language-logo language-accessibility" src={getIconUrl(iconPath)} alt="icon"/>
+            <img
+              className={clsx(
+                {"header__language-logo":true},
+                {"language-html":selectedLanguage===0},
+                {"language-css":selectedLanguage===1},
+                {"language-javascript":selectedLanguage===2},
+                {"language-accessibility":selectedLanguage===3},
+              )}
+              src={getIconUrl(quizData[selectedLanguage].icon)} alt="icon"/>
             <h3 className="header__selected-language">{quizData[selectedLanguage].title}</h3>
+
+            <div className="header__mode-toggle">
+              <img className="header__mode-toggle-icons header__sun-icon--dark" src={DarkSunIcon} alt="dark sun icon"/>
+              <div className="header__mode-toggle-control">
+                <input
+                  className="header__mode-toggle-checkbox"
+                  type="checkbox"
+                  id="mode-toggle"
+                  value={isDarkMode}
+                  onChange={() => {
+                    return setIsDarkMode(!isDarkMode);
+                  }}
+                />
+                <label className="header__mode-toggle-label" htmlFor="mode-toggle"></label>
+              </div>
+              <img className="header__mode-toggle-icons header__moon-icon--dark" src={DarkMoonIcon} alt="dark sun icon"/>
+            </div>
           </div>
         </header>
       }
@@ -37,8 +70,45 @@ function App() {
             <div className="quiz__progress-bar"></div>
           </div>
         </section>
-        <section className="quiz__options-container">
 
+        <section className="quiz__options-container">
+          <label className="quiz__options-label">
+            <input className="quiz__options-radio" type="radio" name="" value="4.5:1"/>
+            <span className="quiz__options-icon-container">
+              <p className="quiz__options-icon">A</p>
+            </span>
+            <p className="quiz__options-value">4.5:1</p>
+            <img className="quiz__options-correct-icon" src={correctIcon} alt="correct icon"/>
+          </label>
+
+          <label className="quiz__options-label">
+            <input className="quiz__options-radio" type="radio" name="" value="4.5:1"/>
+            <span className="quiz__options-icon-container">
+              <p className="quiz__options-icon">A</p>
+            </span>
+            <p className="quiz__options-value">4.5:1</p>
+            <img className="quiz__options-correct-icon" src={correctIcon} alt="correct icon"/>
+          </label>
+
+          <label className="quiz__options-label">
+            <input className="quiz__options-radio" type="radio" name="" value="4.5:1"/>
+            <span className="quiz__options-icon-container">
+              <p className="quiz__options-icon">A</p>
+            </span>
+            <p className="quiz__options-value">4.5:1</p>
+            <img className="quiz__options-correct-icon" src={correctIcon} alt="correct icon"/>
+          </label>
+
+          <label className="quiz__options-label">
+            <input className="quiz__options-radio" type="radio" name="" value="4.5:1"/>
+            <span className="quiz__options-icon-container">
+              <p className="quiz__options-icon">A</p>
+            </span>
+            <p className="quiz__options-value">4.5:1</p>
+            <img className="quiz__options-correct-icon" src={correctIcon} alt="correct icon"/>
+          </label>
+
+          <button className="quiz__next-question-button">Next Question</button>
         </section>
       </main>
     </>
